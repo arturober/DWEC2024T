8d779993818de3ad199f7c0c30d648f5cf86bd14
Arturo Bernal <arturober@gmail.com>
2025-01-17 16:39:55 +0100

    productos parte 2 completa

--- Unidad 2/Parte 2/angular-products/src/app/product-item/product-item.component.ts
+++ Unidad 2/Parte 2/angular-products/src/app/product-item/product-item.component.ts
@@ -1,24 +1,21 @@
 import { DatePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { IntlCurrencyPipe } from '../pipes/intl-currency.pipe';
+import { Component, input, output } from '@angular/core';
 import { Product } from '../interfaces/product';
+import { IntlCurrencyPipe } from '../pipes/intl-currency.pipe';
+import { StarRatingComponent } from '../star-rating/star-rating.component';
 
 @Component({
   selector: 'product-item',
-  imports: [DatePipe, UpperCasePipe, IntlCurrencyPipe],
+  imports: [DatePipe, UpperCasePipe, IntlCurrencyPipe, StarRatingComponent],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css'
 })
 export class ProductItemComponent {
-  product: Product = {
-    id: 1,
-    description: 'SSD hard drive',
-    available: '2016-10-03',
-    price: 75,
-    imageUrl: '/ssd.jpg',
-    rating: 5,
-  };
-  showImage = signal(true);
+  product = input.required<Product>();
+  showImage = input(true);
+  deleted = output<void>();
 
-  deleteProduct() {}
+  deleteProduct() {
+    this.deleted.emit();
+  }
 }
--- Unidad 2/Parte 2/angular-products/src/app/products-page/products-page.component.ts
+++ Unidad 2/Parte 2/angular-products/src/app/products-page/products-page.component.ts
@@ -1,25 +1,17 @@
 import {
-  CurrencyPipe,
-  DatePipe,
-  JsonPipe,
-  UpperCasePipe,
-} from '@angular/common';
-import {
-  ChangeDetectorRef,
   Component,
   computed,
   effect,
-  inject,
-  signal,
+  signal
 } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Product } from '../interfaces/product';
-import { IntlCurrencyPipe } from '../pipes/intl-currency.pipe';
 import { ProductItemComponent } from '../product-item/product-item.component';
+import { ProductFormComponent } from '../product-form/product-form.component';
 
 @Component({
   selector: 'products-page',
-  imports: [FormsModule, JsonPipe, ProductItemComponent],
+  imports: [FormsModule, ProductItemComponent, ProductFormComponent],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css',
 })
@@ -58,17 +50,6 @@ export class ProductsPageComponent {
     p.description.toLowerCase().includes(this.search().toLowerCase())
   ));
 
-  newProduct = {
-    id: 4,
-    description: '',
-    price: 0,
-    available: '',
-    imageUrl: '',
-    rating: 1,
-  };
-
-  #changeDetector = inject(ChangeDetectorRef);
-
   constructor() {
     effect(() =>
       console.log('Imágenes visibles: ' + (this.showImage() ? 'sí' : 'no'))
@@ -79,22 +60,8 @@ export class ProductsPageComponent {
     this.showImage.update((show) => !show);
   }
 
-  changeImage(event: Event) {
-    const fileInput = event.target as HTMLInputElement;
-    if (!fileInput.files?.length) return;
-    const reader = new FileReader();
-    reader.readAsDataURL(fileInput.files[0]);
-    reader.addEventListener('load', () => {
-      this.newProduct.imageUrl = reader.result as string;
-      this.#changeDetector.markForCheck();
-    });
-  }
-
-  addProduct(form: NgForm) {
-    this.products.update(products => products.concat({...this.newProduct}));
-    form.resetForm();
-    this.newProduct.imageUrl = '';
-    this.newProduct.id++;
+  addProduct(product: Product) {
+    this.products.update(products => products.concat(product));
   }
 
   deleteProduct(product: Product) {
