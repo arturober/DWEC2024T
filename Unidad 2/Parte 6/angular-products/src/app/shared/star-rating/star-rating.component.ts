13a8a4b9d590fde08367d9e446847eb07dd447d0
Arturo Bernal <arturober@gmail.com>
2025-03-07 16:52:26 +0100

    login

--- Unidad 2/Parte 6/angular-products/src/app/shared/star-rating/star-rating.component.ts
+++ Unidad 2/Parte 6/angular-products/src/app/shared/star-rating/star-rating.component.ts
@@ -1,14 +1,19 @@
 import { Component, linkedSignal, model } from '@angular/core';
+import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
+import { faStar as faStarFull } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'star-rating',
-  imports: [],
   templateUrl: './star-rating.component.html',
-  styleUrl: './star-rating.component.css'
+  styleUrl: './star-rating.component.css',
+  imports: [FaIconComponent]
 })
 export class StarRatingComponent {
   rating = model.required<number>();
   auxRating = linkedSignal(() => this.rating());
 
+  starEmpty = faStarEmpty;
+  starFull = faStarFull;
 
 }
